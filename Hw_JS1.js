ef8cceebaf9f91f8d6eb047933de7a33e5e3b2ff
str193 <79041079+str193@users.noreply.github.com>
2022-06-14 23:17:39 +0300

    Add files via upload

---
+++ Hw_JS1.js
@@ -0,0 +1,27 @@
+//let name = "Ivan"
+//console.log(name)
+let item_1 = 5
+let item_2 = 3 
+let item_3 = item_1 + item_2
+let item_4 = "Yolochka"
+let item_5 = item_3
+let item_6 = 15
+let item_6_type = item_6
+//console.log(typeof(item_6_type))
+//console.log("item_6 =="+" "+typeof(item_6)) 
+//console.log("item_6_type =="+" "+typeof(item_6_type)) 
+let item_7 = String(item_6)
+//console.log(typeof(item_7))
+let item_7_type = item_7
+//console.log("item_7 =="+" "+typeof(item_7))  
+//console.log("item_7_type =="+" "+typeof(item_7_type))
+let age_1 = 10
+let age_2 = 18
+let age_3 = 60
+if (age_1 < age_2) {
+    console.log("You don’t have access cause your age is " + age_1 + " It’s less then ")
+}  else if (age_1 >=  age_2 && age_1 <  age_3) {
+    console.log("Welcome  !")
+} else if (age_1  > age_3){
+    console.log("Technical work")
+}
